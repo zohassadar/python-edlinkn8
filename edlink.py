1b072347130657c3cbd1b03c42132b4711e0228a
Richard <zohassadar@gmail.com>
2023-11-19 20:19:26 -0700

    add  NesRom

--- edlink.py
+++ edlink.py
@@ -1,7 +1,9 @@
 import os
-import re
-from serial import Serial
 import logging
+import re
+import zlib
+
+from serial import Serial
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
@@ -13,6 +15,14 @@ CMD_MEM_RD = 0x19
 
 BAUD_RATE = 115200
 IDENTIFIER = "EverDrive N8"
+
+
+CMD_TEST = ord('t')
+CMD_REBOOT = ord('r')
+CMD_HALT = ord('h')
+CMD_SEL_GAME = ord('n')
+CMD_RUN_GAME = ord('s')
+
 
 
 if os.name == "posix":
@@ -103,3 +113,59 @@ class Everdrive:
         )
         for i in range(0, STATE_LENGTH, 16):
             print(f"{to_string(state[i:i+8])}   {to_string(state[i+8:i+16])}")
+
+class NesRom:
+    ROM_TYPE_NES = 0
+    MIR_HOR = 'H'
+    MIR_VER = 'V'
+    MIR_4SC = '4'
+    MIR_1SC = '1'
+    MAX_ID_CALC_LEN = 0x100000
+    ADDR_PRG = 0x0000000
+    ADDR_CHR = 0x0800000
+
+    def __init__(self, path):
+        self.rom_type = self.ROM_TYPE_NES
+        if not (path):
+            raise RuntimeError(f'No ROM specified')
+        self.path = path
+        self.rom = bytearray(bytes(open(path, 'rb').read()))
+        self.size = len(self.rom)
+        self.ines = self.rom[:32]
+        self.nes = self.ines[0:3] == bytearray(b'NES')
+        if not self.nes:
+            raise RuntimeError(f"This script only supports nes")
+        self.dat_base = 16
+        self.prg_size = self.rom[4] * 1024 * 16
+        self.chr_size = self.rom[5] * 1024 * 8
+        self.srm_size = 8192
+        if not self.prg_size:
+            self.prg_size = 0x400000
+        self.mapper = (self.rom[6] >> 4) | (self.rom[7] & 0xf0)
+        self.mirroring = self.MIR_VER if self.rom[6] & 1 else self.MIR_HOR
+        self.bat_ram = bool((self.rom[6] & 2))
+        if self.rom[6] & 8:
+            self.mirroring = self.MIR_4SC
+        self.crc = zlib.crc32(self.rom[self.dat_base:])
+        if self.mapper == 255:
+            raise RuntimeError(f"OS mapper not yet supported")
+        self.prg = self.rom[16:16+self.prg_size]
+        self.chr = self.rom[16+self.prg_size:16+self.prg_size+self.chr_size]
+
+        logger.info(f"{self.mapper=}")
+        logger.info(f"{self.prg_size=}")
+        logger.info(f"{self.chr_size=}")
+        logger.info(f"{self.srm_size=}")
+        logger.info(f"{self.mirroring=}")
+        logger.info(f"{self.bat_ram=}")
+        logger.info(f"{self.crc=:08x}")
+
+
+    def get_rom_id(self):
+        offset = len(self.ines)
+        data = bytearray(offset + 12)
+        data[:offset] = self.ines
+        data[offset:offset+4] = bytearray(self.size.to_bytes(4, "little"))
+        data[offset+4:offset+8] = bytearray(self.crc.to_bytes(4, "little"))
+        data[offset+8:] = bytearray((16).to_bytes(4, "little"))
+        return data
