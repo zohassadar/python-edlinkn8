54ef6348045d3f6fd285a4b850843d7efabde77f
Richard <zohassadar@gmail.com>
2023-11-28 16:23:48 -0700

    convert built in testrom to bps

--- edlinkn8.py
+++ edlinkn8.py
@@ -19,7 +19,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 TEST_ROM_LEN = 40976
-TEST_ROM_SHA1SUM = "ea914304489deade0a3ef598b4a7e5dc0d558c59"
 
 STATE_LENGTH = 0x100
 
@@ -458,7 +457,8 @@ def apply_ips_patch(rom: bytearray, patch: bytes, sha1sum: str = "") -> bytearra
 def get_test_rom() -> bytearray:
     rom = bytearray(TEST_ROM_LEN)
     patch = base64.b85decode(fifo_testrom)
-    return apply_ips_patch(rom, patch, TEST_ROM_SHA1SUM)
+    bps_patch = BPSPatch(patch)
+    return bps_patch.patch_rom(rom)
 
 
 def open_rom_with_patch(rom_file: str, patch_file: str, sha1sum: str = "") -> bytearray:
@@ -484,82 +484,205 @@ def send_fifo(everdrive: Everdrive, *numbers):
     payload = bytearray(n & 0xFF for n in numbers)
     everdrive.write_fifo(payload)
 
+TEST_ROM_LEN = 40976
 
 fifo_testrom = (
-    b"P(f5fNB{r;00&M*QyKyR0{{RJ1A3(m&;+Fp@H6HYsVj}}KuQ0N@Ia{|jqpH8|BdiKr45bnK&1|i@Ia*u"
-    b"qz<MH@CJ?WK+4eih`|5Q^<d@~t?@vG3Sj0Jt?)pF3t;9KsQ`r)g%+*xK*>SS46X1$<`vNL<`zs;fMBTs"
-    b"jTS(K1xk$;K&=))N+t!6_@xC%MFu4Wg#~DZ24F~vNSH|G0;vIo2dN;91|X@3jRqj152^o+2O!E2`l%p|"
-    b"1|X@SjRqj1lAwVg(1E27jRzp55RC^QqMD$AAkcxO5se2Rr4o$?Af*$H2Oy;sjRzp9A&mwgsUeL9Aflt7"
-    b"fgsR<r3xV9fvF*l1|X@SjRqj1v7mt<(1E23Amf3lB8>(hsUeL9AfmsZfgsR<r4}ILfu$87<AJFnjRqj8"
-    b"p^XM0qRpUzAkcxO7a-$-rU<=+fsF<ry@-L01|X^MjRzp53Gf)HAdLnfrU|`^ff#9r7-b2K1|X^NjRzp9"
-    b"0F4D8jRhd7h>ZXssSb?+ARuUfXsBqaXh0w#AtIq6p&<YO2nYxWAX8;-WFRE}AY*TJZge0d{~%IjVPqgA"
-    b"G%#i{JtY4iQe|OeAS5&}W-vV@{~&B-ZYXheWp!mKJtY4iB>#p0gaM!cu>i^c@CJ<sAjr`4U`R?zN=l6f"
-    b"AZRHMjRzoLr3lbPr3EPh@Ce2U5Co|Pg$bnvDFN^d<_V<<$p_E`sQ`rur3vsMrU|75DG2Zg_71fUDG!wn"
-    b"r3EPj@CeopwGJr{l@3f>f~5s12=EBT2oMCR1BD2s1t|pZ4CV->2+0J{1gQXp2&EAU3JMAZ5`_+>6AB6n"
-    b"3I!B}4W$nX3JMAZ5QPk-1u1~=1*Hfemx8GPg$Jbv@cc~jfdGI!fJA^yfC{R;kAj5&y^w;10c-#Oc-W#q"
-    b"ju$|p|C;EI03eP5AdV0~ECL`b0w56kij@GJ00Er<0-XQ@od5)#00o@@2Au#0=+NdY0w56ksUVF8AgKV2"
-    b"1|XsUssD`!An4HgsUeL9AgKV21|XsUssD`!An4HgsUnRAAgKV21|XsUssD`!An4HgsUwXBAgKV21|Xuq"
-    b"ssD`!Aj;7CsXvVdAgKV21|XsUy;+0F|L_Nm2O#K7K7*-<jQ}929*qGYO!9#bFfcF=FfcF=FfcF=FfcF="
-    b"FfcF=FfcF=FfcF=FfcIx0DuYs1&DyAf`fq`GdDjwGadjQGdDjwGadj891IK$3?2Xs91IK$3?2X;GY1YF"
-    b"I6nX$GY1YFI6nX$GXo9-GadjQGXo9-Gadj24i+Xq1_l5I4i+Xq1_l5>Fg^nVGadjxFg^nVGadjQGcZ0g"
-    b"GadjQGcZ0gGadjxBLfBo3=9B2BLfBo3=9AsGcz7DGadjQGcz7DGadjQGcz9pGadjQGcz9pGadjQGc!Lk"
-    b"Gcy1lGc!LkGcy2wGc!IjGkySmGc!IjGkyRcGchnQF&+RPGchnQF&+SZGcz+YGkySmGcz+YGkySnF)%zZ"
-    b"F@FGmF)%zZF@FGmF)%zZFmM2WF)%zZFmM2ZAONly7#J8B02lxm7#J8B02lylY)AkA0001NY)AkA0000e"
-    b"e0(T;d?)}Ye0(T;d?)}AJa9N1cn|;(Ja9N1cn|=_&Kx*!=EeZV&Kx*!=EeXxWN<ocW;y^kWN<ocW;y^E"
-    b"7!Uve0000O7!Uve0000OFfcGMFc<(BFfcGMFc<(B3=9km3>W|y3=9km3>W|aY&iZnYybcNY&iZnYybcN"
-    b"7#MyS7ytkO7#MyS7yyFQ01y}$2mk;8000;m2mk;806YMK>i_{f0D|=Z0vH$og8c&i7#IKy3>+9ZFfafN"
-    b"3>+9ZFfaf-W^R6VW;_5qW^R6VW;_5GI2afh7(4(NI2afh7(4(xW)2)UaDD(hW)2)UaDD(hW(FJvW;_5q"
-    b"W(FJvW;_5494t(J3=9Aa94t(J3=9B%V0;D!W;_6XV0;D!W;_5qW?+0~W;_5qW?+0~W;_6XMg|NF7#IM4"
-    b"Mg|NF7#ILNW@bEQW;_5qW@bEQW;_5qW@bJHW;_5qW@bJHW;_4@7#IK;7ytkO7#IK;7ytkO7#IK;7zh9W"
-    b"7#IK;7zh9i7%*Tk7z_Xm7%*Tk7z_Xa06YLZ0000006YLZ0000m7z_ps7%%`Z7z_ps7%%`lW(Eux02lx~"
-    b"W(Eux02lx~W^Qg^VmtsmW^Qg^VmtsmW@dh7W@Z39W@dh7W@Z5VW@db5X8ZvBW@db5X8Zs?W@2DqVmtso"
-    b"W@2DqVmtu+W@ct)X8ZvBW@ct)X8ZvDVqkb+V*UXBVqkb+V*UXBVqkb+VBi4$Vqkb+VBi3J#=y?T#(V&L"
-    b"#=y?T#(V%~W@dh7W@Z3pW@dh7W@Z397#J8B7(4(x7#J8B7(4(z3=9kmOgI2O3=9kmOgI4cW^8zDX7&K~"
-    b"W^8zDX7&JhFfcGMGJXJfFfcGMGJXKY?*7)s#>N1~?*7)s#>N0<W_EsVW@Z3pW_EsVW@Z39W@ct)W;_5q"
-    b"W@ct)W;_7=W@da~VDJF^W@da~VDJDuW@ct)Y&rluW@ct)Y&rn^W@db5X669=W@db5X667sVqiQ5MtlH1"
-    b"VqiQ5MtlH%S{N7@7(4)eS{N7@7(4)GW@ct)W;_69W@ct)W;_69W@ct?JQx6GW@ct?JQx7R#>UqEY(xOY"
-    b"#>UqEY(xNNW;_@?W@Z3pW;_@?W@Z3pW^Ozf7#IL%W^Ozf7#IM4P8>LJVtxRAP8>LJVtxQTFfcGMFgyS}"
-    b"FfcGMFgyS-FgO?-3=9A;FgO?-3=9A~3=9km3_Jil3=9km3_Ji3IBWm_0000GIBWoh1^@wm0EP|#4t@YI"
-    b"I2;TB0000mI2;TBh9Uv2JO(~yJ^%m!JO(~yJ^)}~d}d~5d;nlzd}d~5d;kCdJYryCJOBUyJYryCJOBm;"
-    b"K4xZSJ^%&=K4xZSJ^%m!JZ64iJOBUyJZ64iJOB<D7(5slJOB<D7(5slJOBUyK4xY<20Q=&K4xY<20UP3"
-    b"Y<6a5W&mJdY<6a5W&jufI2afhJOCH~I2afhJOB&;91IK$3^)t`91IK$3^-t5WNdhBW&mJdWNdhBW&k)C"
-    b"7#J8BJODTt7#J8BJOBUy?AF%S)&Kwi?AF%S)&KwiTxMowW&i*HTxMowW&i*HJZ5HQJOBUyJZ5HQJOBUy"
-    b"d}d~RU|;|Md}d~RU|;|MK4xY<1_l5CK4xY<1_l5CY<6H^U;qFBY<6H^U;qFBJYYNqJOBUyJYYNqJOD5-"
-    b"d@wLD8~`vdd@wLD8~^|SW@ct)IsgCwW@ct)IsgCwW@ctQ7ytkOW@ctQ7ytkOMpjl<J^%m!Mpjl<J^%m!"
-    b"W;_@?W&i*HW;_@?W&i*HW@bJH8~^|SW@bJH8~^|Sd>A+|d;kCdd>A+|d;lCUFkmn+8~_|JFkmn+8~}&V"
-    b"0000G7yyUh05CWV3<eAgH~=^d3<eAgH~;_uGJZ?|00000GJZ?|002BfnW3ebLOeV|nW3ebLOcME5da-P"
-    b"U~qVFU_bysU~qVFU_bx>e?A-t00000e?A-t0FTfB000pG0FUSZ000pG0FV#>{~j|pKRYuX03I_pKRYuX"
-    b"01O-q3=9k&01O-q3=9k&03I_34jec?03I_34jec?03I^~4g)hD03I^~4g)hD00s^gCO-xS00s^gCO-xS"
-    b"06#E30|PT206#E30|PT203I_iJ~J~O03I_iJ~J~O06!xG1_lfa06!xG1_lfa03I_l9y2o@03I_l9y2o@"
-    b"03I_l9|JQU03I_l9|JQU03I_lKQl8k03I_lKQl8k0DdzwJ~K0Z0DdzwJ~K0Z03S0kFfcJ503S0kFfcJ5"
-    b"0DdzwGcz-O0DdzwGcz-O0DmzsJTNhT0DmzsJTNhT0DmzsJTNeD0DmzsJTNeD0Fod8t{4~?7#IK;02mk;"
-    b"7#IK;0BmeX000000BmeX0000004RKXD13Y<04RKXD13Y<01!NII2?Eo01!NII2?Eo0LIQ7IB@310LIQ7"
-    b"IB@31061iDI&5Y-061iDI&5Y-02mk$0000002mk$0000002nYZFfcF}02nYZFfcF}02mAm3=9ky02mAm"
-    b"3=9ky003+_{y1y^003+_{y1y^000;mei#@4000;mei#@4lGFeY7#Iiu000007#Iiu0000y0Fvtf0XzVb"
-    b"^#B4G7yy#}0{$2n01ONq7&tI601ONq7&tI606b=Hes*R&06b=Hes*R&02nwJ7#J8l02nwJ7#J8l06b<6"
-    b"95`@(06b<695`@(06b;}90q1Q06b;}90q1Q01O-~OnwXu01O-~OnwXu0DfS61_ow40DfS61_ow406b=3"
-    b"d}d}m06b=3d}d}m0DeXW3=9|;0DeXW3=9|;06b=9JZ5G*06b=9JZ5G*06b=9J_cqy06b=9J_cqy000;m"
-    b"02mkm000;m02mkm000;m02mku000;m02mku01Ox~U@#aA01Ox~U@#aA0000y06YKy0000y06YKy05BK~"
-    b"1`HT505BK~1`HT506b;}3>W|y06b;}3>W|y06b=HZeU_O06b=HZeU_O06b=9er9H706b=9er9H70Q_cV"
-    b"d}e0+0Q_cVd}e0+06u17U|?cA06u17U|?cA0Q_cVW@cvm0Q_cVW@cvm0RCcNcwl1w0RCcNcwl1w0RCcN"
-    b"cwk`Q0RCcNcwk`Q0DQ*4&c?=k0DQ*4&c?=k0A^-per9H70A^-per9H706Z8N7#J8l06Z8N7#J8l06q*1"
-    b"3=B*-06q*13=B*-0QP2Vcx-0&0QP2Vcx-0&0C+GkFfcNH0C+GkFfcNH0LJeA*2c!h0LJeA*2c!h0A^-("
-    b"er{%F0A^-(er{%F06b=9W@ctQ06b=9W@ctQ0Q_cVd|+Vk0Q_cVd|+Vk06b=9W@c<U06b=9W@c<U0Q_cV"
-    b"d}e0m0Q_cVd}e0m06t=1JO)O506t=1JO)O50Df8+7#J8l0Df8+7#J8l0A^-pW@ctQ0A^-pW@ctQ0A^-p"
-    b"W^Ozf0A^-pW^Ozf0LI3~*8Xfn0LI3~*8Xfn0A^-97(8ZX0A^-97(8ZX0A^-xJQx@l0A^-xJQx@l0Dev!"
-    b"IB;Tq0Dev!IB;Tq06Z`-FfcGY06Z`-FfcGY05C8(7#s`?05C8(7#s`?06Yu~3=9lB06Yu~3=9lB01!BA"
-    b"0000001!BA0G0*-0e%3M4gd~*05CWl3;+NC05CWl3;>oQ0j)d+K4v}u002A&K4v}uU|@V^W@dZ<U|@V^"
-    b"W@dZ<002B<U}8J~002B<U}8J~1_nN6W@bJB1_nN6W@bJB002B@eqcNR002B@eqcNR4j33b7#KVN4j33b"
-    b"7#KVN002H_W<CZy002H_W<CZyU|?)^W@cspU|?)^W@csp7yvjJ7#KVN7yvjJ7#KVN3;-Mq3=9l73;-Mq"
-    b"3=9l7U|?iycx+|>U|?iycx+|>I2afh7#KVNI2afh7#KVN008XP*4EYl008XP*4EYl003NOW@csp003NO"
-    b"W@csp002B@W@bD9002B@W@bD9004YuW_(~^004YuW_(~^002H_W<CZ6002H_W<CZ6003-uU|?VX003-u"
-    b"U|?VX002B-JO(@f002B-JO(@fFfe>DFfbedFfe>DFfbed003rYW@b77003rYW@b77003rYW;_@G003rY"
-    b"W;_@G002f-R#rX$002f-R#rX$003q@7(8YG003q@7(8YG003rYJ_Z~B003rYJ_Z~B004X#I52zw004X#"
-    b"I52zw9566oFfbed9566oFfbedm(TzJ01y}em*4;}I1CI13=B8`I1CI13=B8`001(6OaK4?001(6OaK4?"
-    b"JVKeFrI|uJJVKeFrI|uJ0G|;69YA1kcyM4q06<`HcyM4q004hJ90&ja004hJ90&lP&;S4c5dQ$5=l}o!"
-    b"5dTF_Mg"
+    b"LQqpN5I=zsKY@UmPDN810s#Y}fYqf9&;+Fp@H6HYsVj}}KuQ0N@Ia{|pSXpk4Udk24v&fD45SXG4e$ny"
+    b"@IcDY`iQ{)(Dh*E7p?I?g$j?jeSz>mg$rQj7pVY+6@?b9@j%Hz&<w5cK;{+D^5zyyRDfWq0gV<wg#}8D"
+    b"7C@~QKuRVBkoctqNJR!E1%(A@g$7_qib$A9<^rh!g$Jo1jRqj8h>Zpyq7SM6jRzpg5c-!WfPtZpg2j@c"
+    b"fgsR<r4Nk<Af*tE2Oy%Fm#TpgkAi^`kD7rKkAjsHjRzp9A&>u$3V?;8qnF=}3LxQum&lXtfw7l@feVlB"
+    b"fg+=;fxnl@l@=i3fu$9r_>--H&6lpV7a-w*rU<=+fsF<ry@-#-p{ekV2Oy;h@EDU{fxV^)y^DbuX@?kP"
+    b"35^CIsqv2lfRzA^1t5(Dj}L%pfB+z=4vhgIAZUPSsA#HaKp-I@BB3FnApigf2nYxuQ)O;sAS3`FV{dhC"
+    b"bRZ-EAW~&vWFRCoFlI45uMC5MFpq+zY-MgJadl;NWhsw|DS;#ah5&>Cpa8J|@CJ<sAjr`6U`R?zN=l6f"
+    b"AZRHMjRzoLr3lbPr3EPh@Ce2U5Co|Pg$bnvDFN^d<_V<<$p_E`sQ`rur3vsMrjH(g#R%{S_71fUDG!wn"
+    b"r3EPj@CeqMxQR?zf|s40#t0AusRM-wlaQGV<_M(-$pn+Xu?VFR3JMAe1rmi0r4y47fRPl14W$p0iy49t"
+    b"g$$(yDS+?=r3fIEf~f$72c-w_{7mqH0DwGzM1V|y3aY)0f`tIRj)H{&Yybdw*rGs=7eJ!_n&^%IAdUeb"
+    b"ju1dB0w62`AQ1bCl>nUp0i6H>od5%!00f-?1)Tr}od5^u(B_jifR|f`fdG$!fdG?&iRjS!m-4#?hm%)^"
+    b"yH<vQBcp<Wz?W%=jnMk3Kc`BFnY~wo$^Y;NkIjipJcE}5g{dBm0U%89fe$b+Fz-Nyf&T_$hLg33fTV(h"
+    b"fgUqAKRYuX0G}Fhf|U#$3=9kmr;M2%GY1YFI6te7i31J;lgpRTnFbCPCO-xSqmGR~Fg^p1sh_Zs9y2gL"
+    b"GpmZ3KO+MM1`G_Nj)@*KGmnt3i-8}P-=pA-KQl8kqllS)Gc!IjGk&9vnIAJTFfcJ5qmGNAi>{Nuldze8"
+    b"F)%zZF@Lv?g)nfVgg=KJhnW}{7#J7;7^5nOiEM00lShZ2Ooxdme0-0Pql=jkJa9N1co3tGna0i>IB@31"
+    b"qmG$4WN<ocW;&yei5M6Vn>&D;K!AZTkC2OBfS*u+k8g;945x62l>lrw{y1!pXMme>fRPv&ei#_5ijxU|"
+    b"fe5b(fPp-lF^ZouijT9SvY8AF92ht-Fr$u{JZ5fwc4j=Ij+GcV7#J8Br;M3AW)2)UaDJ<fi3S`7lgpRT"
+    b"nG75(OnwXuqmGS!V0;FTsh_ZsJZ4~gW~+*sentii3>X-rj)^>GW{;4si-A6u-<RNzS%I7Rt5AZ02&00T"
+    b"3>YwAFc=J@j*oAGmxY&df{Uq*1`HT5qp+1cW(Eux0FPCGpJ0HEZf;;=lWBsRaDtI$er9H7ql%gQW@db5"
+    b"X8fa$jXq{#V2{b4(2Jpqu9Lr$u$lg1V0d6+{<n^WVBn*KnS92;&c?=kqmGN~lh>a>fPp-dVTge|qhyGg"
+    b"J`4;D3`{tqj+ypmY<O&D_M?uOcrY+9Ffx9lj+w^p{?^9E#-omrW@dJNZj&c~moR{VJd;m>lkt;4f{!7B"
+    b"iC|#xqb7orp@nQZqokK%f`R6vVuG1IVqiQ5Mtq}=g??I>J%OJ<fs+q|fo8W5gNbfD7^8-n#>U3h{%k~}"
+    b"j*(_&JQzHaXM&$_f{VR@7>|>e(3yTt95`@dexr_oJd;n0fjpy9i<vMmI2arZ45N;LJd<yWfjpyfi-8c6"
+    b"S&M-Hqg;!ZX_kS0qiU9!FgP3x0001^j+p=eJO(~yKBJDAU|@V^W@db&j*Fp<VqjuCqo|n%20mtHW<H~i"
+    b"i=l~TeqfV}ldzQz7#KVl7^jS#002H_W<CZypOBqkU~G0~W@Z4Nkck)oIG1aTn{bSU3;>gVn1MK-1euj!"
+    b"U}S7~Y>&m4(34M%g%~`eQjM7a0PNP**4C?zg<NKj9e}4GfRA;Cmqdh@FoA)5lVgUbWQLP5fq@36GJ%O~"
+    b"c3_X0qn?dCU_1tsX@Zk*f|)Qdd@wLD9HWkb0F!fufjYN#hJ`#BtAvq8R#sL%tBR9zhOcsliDo_qm;09h"
+    b"fRTI{I52#pij5pFFkp{KfS*u+lV5_XY=WIQ3=9Sg3^)Lvf{_3KGJZ^x1(TN!lbt+5nW3ebLOh?41Gj&="
+    b"nLuE0cyM4qqYS%^0DnFllXtJ5ey?|feZ7JIy9~bnufI=%fBplzM+U-2;zCt>PBw!y"
 )
 
+"""
+BPS Code from https://github.com/mgius/python-bpspatcher
 
+"""
+import binascii
+import enum
+import io
+
+ENDIAN = "little"
+
+
+class Action(enum.IntEnum):
+    SourceRead = 0
+    TargetRead = 1
+    SourceCopy = 2
+    TargetCopy = 3
+
+
+def convert_uint(b: bytes):
+    return int.from_bytes(b, ENDIAN, signed=False)
+
+
+def read_number_io(b: io.BytesIO) -> int:
+    data, shift = 0, 1
+
+    # this was basically directly copied from the bps_spec
+    while(True):
+        x = b.read(1)
+        if len(x) == 0:
+            return None
+        x = convert_uint(x)
+        data += (x & 0x7f) * shift
+        if (x & 0x80):
+            break
+        shift <<= 7
+        data += shift
+
+    return data
+
+
+def read_number(b: bytes) -> tuple:
+    """ Read a number that starts at the beginning of the bytes
+
+    returns a tuple of the number read and remaining bytes
+    """
+    bio = io.BytesIO(b)
+    data = read_number_io(bio)
+    return data, bio.read()
+
+
+class InvalidPatch(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+class BPSPatch(object):
+    MAGIC_HEADER = "BPS1".encode("UTF-8")
+
+    def __init__(self, patch: bytes):
+        header = patch[:4]
+
+        if header != self.MAGIC_HEADER:
+            raise InvalidPatch(f"Magic header {header} is incorrect")
+
+        self.source_checksum = convert_uint(patch[-4*3:-4*2])
+        self.target_checksum = convert_uint(patch[-4*2:-4*1])
+        self.patch_checksum = convert_uint(patch[-4*1:])
+
+        calculated_checksum = binascii.crc32(patch[:-4])
+
+        if self.patch_checksum != calculated_checksum:
+            raise InvalidPatch(
+                f"Patch Checksum {self.patch_checksum} does not match "
+                f"actual checksum {calculated_checksum}"
+            )
+
+        remainder = patch[4:]
+
+        self.source_size, remainder = read_number(remainder)
+        self.target_size, remainder = read_number(remainder)
+        self.metadata_size, remainder = read_number(remainder)
+
+        self.metadata = remainder[:self.metadata_size].decode("UTF-8")
+
+        # actions is everything else other than the header and footer
+        self.actions = remainder[self.metadata_size:-12]
+
+    def patch_rom(self, source: bytes) -> bytes:
+        if len(source) != self.source_size:
+            raise InvalidPatch(
+                f"source size {len(source)} does not match "
+                f"expected {self.source_size}")
+
+        source_checksum = binascii.crc32(source)
+        if source_checksum != self.source_checksum:
+            raise InvalidPatch(
+                f"source checksum {source_checksum} does not match "
+                f"expected {self.source_checksum}")
+
+        target = bytearray(self.target_size)
+
+        actions = io.BytesIO(self.actions)
+
+        output_offset = 0
+        source_relative_offset = 0
+        target_relative_offset = 0
+
+        while(True):
+            action = read_number_io(actions)
+            if action is None:
+                break
+
+            command = action & 3
+            length = (action >> 2) + 1
+
+            print(f"Command {command}, length {length}")
+
+            if command == Action.SourceRead:
+                # consume some number of bytes from source file
+                target[output_offset:output_offset + length] = \
+                    source[output_offset:output_offset + length]
+                output_offset += length
+
+            elif command == Action.TargetRead:
+                # consume some number of bytes from patch file
+                target[output_offset:output_offset + length] = \
+                    actions.read(length)
+                output_offset += length
+
+            elif command == Action.SourceCopy:
+                # consume some number of bytes from source file, but from
+                # somewhere else.  This action seems unnecessarily complicated
+                data = read_number_io(actions)
+                source_relative_offset += (-1 if data & 1 else 1) * (data >> 1)
+                target[output_offset:output_offset + length] = \
+                    source[
+                        source_relative_offset:source_relative_offset + length]
+
+                output_offset += length
+                source_relative_offset += length
+
+            elif command == Action.TargetCopy:
+                # consume some number of bytes from the target file
+                data = read_number_io(actions)
+                target_relative_offset += (-1 if data & 1 else 1) * (data >> 1)
+                # unfortunately it is not safe to optimize this, as one of the
+                # documented use cases is to write a single byte then duplicate
+                # that byte over and over filling out an array.
+                for _ in range(length):
+                    target[output_offset] = target[target_relative_offset]
+                    output_offset += 1
+                    target_relative_offset += 1
+
+        target_checksum = binascii.crc32(target)
+
+        if target_checksum != self.target_checksum:
+            raise InvalidPatch(
+                f"target checksum {target_checksum} does not match "
+                f"expected {self.target_checksum}")
+
+        return target
+
+
+"""
+end BPS Code
+"""
 if __name__ == "__main__":
     main()
